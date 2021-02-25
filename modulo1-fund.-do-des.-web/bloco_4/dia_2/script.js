ec4d3d1639f2af0ba25f24a4445229b1d4b07b83
Roger Franco <rogerfrancogalt@gmail.com>
2021-02-25 14:12:11 -0400

    primeiro commit da aula

---
+++ modulo1-fund.-do-des.-web/bloco_4/dia_2/script.js
@@ -0,0 +1,32 @@
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+// menu[4] = 'contatos';
+
+// menu.push('contatos'); /* ele acrescenta ao ultimo */
+// menu.unshift('casa'); /* ele acrescenta ao inicio */
+
+
+// // console.log(menu.length); /* quantidade de elementos */
+// // console.log(menu.sort()); /* ordem aufabetica */
+// // console.log(menu[1]); 
+// // console.log(menu[2]); 
+
+// for (let index = 0; index < menu.length; index += 1) {
+//   console.log(menu[index]);
+// }
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// let searchForFirstTask = tasksList[0];
+// console.log(searchForFirstTask);
+// // Tomar café
+
+// let searchForLastTask = tasksList[tasksList.length - 2];
+// console.log(searchForLastTask);
+// // Brincar com o cachorro
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+menu.push('contatos');
+
+console.log(menu);
