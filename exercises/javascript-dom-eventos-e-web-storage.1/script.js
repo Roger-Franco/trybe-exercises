c3bca3e5ec61c89eb97f2ac397271d232157e31d
Roger Franco <rogerfrancogalt@gmail.com>
2021-03-03 23:26:21 -0400

    exercícios completados

---
+++ exercises/javascript-dom-eventos-e-web-storage.1/script.js
@@ -0,0 +1,12 @@
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = 'rgb(254, 159, 133)';
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = 'rgb(249, 219, 94)';
+document.getElementById("header-container").style.backgroundColor = 'rgb(11, 176, 105)'; /* Aqui nao precisou do [0] */ 
+document.getElementById("footer-container").style.backgroundColor = 'rgb(1, 53, 52)'; 
+// document.getElementById("bordo").style.backgroundColor = 'rgb(165, 0, 243)'; /* Aqui nao precisou do [0] */ 
+document.getElementsByClassName("roxo")[0].style.backgroundColor = 'rgb(165, 0, 243)';
+document.getElementsByClassName("roxo")[1].style.backgroundColor = 'rgb(165, 0, 243)';
+document.getElementsByClassName("preto")[0].style.backgroundColor = 'rgb(35, 38, 38)';
+document.getElementsByClassName("preto")[1].style.backgroundColor = 'rgb(35, 38, 38)';
+
+
+// eu tive que fazer algumas inserções, como exemplo as classes roxo e preto...
