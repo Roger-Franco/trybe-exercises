a135e6b32bb309d28e248e98610088913e7ee6e5
Roger Franco <78622773+Roger-Franco@users.noreply.github.com>
2021-08-16 18:12:36 -0400

    dia 3 bloco 24

--- exercises/modulo-3/bloco-24/dia-3.js
+++ exercises/modulo-3/bloco-24/dia-3.js
@@ -1,3 +1,156 @@
+// Exercício 1 : Utilizando o operador $all , retorne todos os filmes que contenham action e adventure no array category .
+db.movies.find(
+    {
+      category: {
+        $all: ["action", "adventure"]
+      }
+    }
+  ).pretty();
+
+  // Exercício 2 : Agora retorne os filmes que contenham action no array category e possuem nota do IMDB maior do que 7 .
+  
+  db.movies.find(
+    {
+      category: {
+        $all: ["action"]
+      },
+      imdbRating: { $gt: 7 }
+    }
+  ).pretty();
+
+  // Exercício 3 : Adicione um array chamado ratings ao filme Batman com os seguintes valores: [85, 100, 102, 105] . Dica: lembre-se do operador $each visto no dia anterior.
+  
+  db.movies.updateOne(
+    { title: "Batman" },
+    {
+      $push: {
+        ratings: {
+          $each: [85, 100, 102, 105]
+        }
+      }
+    }
+  );
+
+  // Exercício 4 : Adicione um array chamado ratings ao filme Godzilla com os seguintes valores: [78, 52, 95, 102] .
+  
+  db.movies.updateOne(
+    { title: "Godzilla" },
+    {
+      $push: {
+        ratings: {
+          $each: [78, 52, 95, 102]
+        }
+      }
+    }
+  );
+
+  // Exercício 5 : Adicione um array chamado ratings ao filme Home Alone com os seguintes valores: [200, 99, 65] .
+  
+  db.movies.updateOne(
+    { title: "Home Alone" },
+    {
+      $push: {
+        ratings: {
+          $each: [200, 99, 65]
+        }
+      }
+    }
+  );
+
+  // Exercício 6 : Retorne todos os filmes com ratings maior do que 103 , exibindo apenas os campos title e ratings .
+  
+  db.movies.find(
+    {
+      ratings: {
+        $elemMatch: { $gt: 103 }
+      }
+    },
+    {
+      _id: 0,
+      title: 1,
+      ratings: 1
+    }
+  ).pretty();
+
+  // Exercício 7 : Retorne todos os filmes com ratings entre 100 e 105 , exibindo apenas os campos title e ratings .
+  
+  db.movies.find(
+    {
+      ratings: {
+        $elemMatch: { $gte: 100, $lte: 105 }
+      }
+    },
+    {
+      _id: 0,
+      title: 1,
+      ratings: 1
+    }
+  ).pretty();
+
+  // Exercício 8 : Retorne todos os filmes com ratings entre 64 e 105 e que é divisível por 9 , exibindo apenas os campos title e ratings .
+  
+  db.movies.find(
+    {
+      ratings: {
+        $elemMatch: { $gte: 64, $lte: 105, $mod: [9, 0] }
+      }
+    },
+    {
+      _id: 0,
+      title: 1,
+      ratings: 1
+    }
+  ).pretty();
+
+  // Exercício 9 : Retorne os filmes da categoria adventure e com ratings maior do que 103 , exibindo apenas os campos title , ratings e category .
+
+db.movies.find(
+  {
+    ratings: {
+      $elemMatch: { $gt: 103 }
+    },
+    category: { $all: ["adventure"] }
+  },
+  {
+    _id: 0,
+    title: 1,
+    ratings: 1,
+    category: 1
+  }
+).pretty();
+
+// Exercício 10 : Retorne somente o título de todos os filmes com dois elementos no array category .
+
+db.movies.find(
+  { category: { $size: 2 } },
+  { _id: 0, title: 1 }
+).pretty();
+Exercício 11 : Retorne somente o título de todos os filmes com quatro elementos no array ratings .
+Copiar
+db.movies.find(
+  { ratings: { $size: 4 } },
+  { _id: 0, title: 1 }
+).pretty();
+
+  // Exercício 12:
+  // Busque os filmes em que o módulo 5 do campo budget seja 0 e que o array category tenha tamanho 2 .
+
+  db.movies.find({ budget: { $mod: [5,0] } ,category: { $size: 2 } }).pretty();
+
+  // Exercício 13 : Retorne os filmes da categoria "sci-fi" ou que possua o ratings maior do que 199 , exibindo apenas os campos title , ratings e category .
+
+  db.movies.find({ $or: [
+    {category: { $all: ["sci-fi"]}},
+    {ratings: { $elemMatch: { $gt:199 } } }
+  ]}, { _id: 0, title: 1, ratings: 1, category: 1}).pretty();
+
+  // Exercício 14 : Retorne os filmes em que o ratings possua tamanho 4 e que seja da category "adventure" ou "family" , mas que não tenha o imdbRating menor que 7. (resposta tem que ser batman) 
+
+  db.movies.find({ $and: [
+  { ratings: { $size: 4 } },
+  { category: { $in: ["adventure", "family"] } },
+  { imdbRating: { $not: { $lt: 7 } }}
+]});
 // Exercício 15 : Adicione o campo description no filme Batman com o seguinte valor: "The Dark Knight of Gotham City begins his war on crime with his first major enemy being Jack Napier, a criminal who becomes the clownishly homicidal Joker." .
 
 db.movies.update(
