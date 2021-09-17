54d3d4fdcf814f3de343e4e8a4a3b8a65810db4e
Roger Franco <78622773+Roger-Franco@users.noreply.github.com>
2021-09-17 13:43:48 -0400

    parte 1 exercicios do segundo dia do bloco 28

---
+++ exercises/modulo-3/bloco-28/dia-1/hello-jwt/controllers/index.js
@@ -0,0 +1,12 @@
+const ping = require('./ping');
+const login = require('./login');
+const topSecret = require('./topSecret');
+
+const me = require('./me');
+
+module.exports = {
+  ping,
+  login,
+  me,
+  topSecret,
+};
